93349b9574070f46b7e76a3a989df6f5f07485a6
BishrBinAli <bishrkc@gmail.com>
2022-10-28 00:58:12 +0100

    Define what happens if letter is not in word

--- milestone_4.py
+++ milestone_4.py
@@ -18,6 +18,11 @@ class Hangman():
                 if value == guess:
                     self.word_guessed[key] = guess
             self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+        self.list_of_guesses.append(guess)
 
     def ask_for_input(self) -> None:
         while (True):
@@ -28,7 +33,6 @@ class Hangman():
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
-                self.list_of_guesses.append(guess)
 
 
 word_list = ["Apple", "Banana", "Pear", "Orange", "Strawberry"]
