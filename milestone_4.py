6f490f8e90b90d1f5dc2fd11ddd45521d85d9027
BishrBinAli <bishrkc@gmail.com>
2022-10-28 00:39:19 +0100

    Create methods in Hangman class to run the checks

--- milestone_4.py
+++ milestone_4.py
@@ -9,3 +9,24 @@ class Hangman():
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
+
+    def check_guess(self, guess) -> None:
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+    def ask_for_input(self) -> None:
+        while (True):
+            guess = input("Please enter a single letter: ")
+            if (len(guess) != 1) or not (guess.isalpha()):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
+word_list = ["Apple", "Banana", "Pear", "Orange", "Strawberry"]
+test_game = Hangman(word_list)
+test_game.ask_for_input()
